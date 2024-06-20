c9f520361ef6d19d17d605123803e6931bb88d1e
CaioAdania <caioadania@gmail.com>
2024-06-20 11:24:59 -0300

    estudos Enum

---
+++ EstudosEnum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudosEnum.Entities.Enums
+{
+    enum OrderStatus : int //derivação de tipo
+    {
+        PendingPayments = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ EstudosEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using EstudosEnum.Entities;
+using EstudosEnum.Entities.Enums;
+
+namespace EstudosEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayments
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayments.ToString(); //conversão de enum para string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); //conversão de string para enum
+
+            Console.WriteLine(os);
+
+            Console.WriteLine(txt);
+        }
+    }
+}
